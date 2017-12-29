365efbcdeb66f4cfcbd742f18aaea9acd6f3f774
Davide Bizzi <d.bizzi@appleseed-mail.it>
2017-12-29 18:41:31 +0100

    added menuitems and pet feature

---
+++ src/components/CareMenuItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import MenuItem from './MenuItem';
+import axios from 'axios';
+
+
+
+export default class CareMenuItem extends React.Component {
+	constructor(props) { 
+		 super(props); 
+		 this.pet = this.pet.bind(this);
+	}
+	pet(e){
+		let url = "http://localhost:8080/pet/";
+		console.log(url);
+		axios.post(url,{
+			uid : this.props.uid,
+			type : "pet"
+		}).then(
+			(response) =>  {
+				if (response.error) console.log(false);
+				console.log(response.data)
+			}) ;
+	}
+	render() {
+		let actions = [
+			{ onClick : this.pet, name : "PET" }
+		]
+		return <MenuItem actions={actions} title="Care" uid={this.props.uid}/>
+	}
+	
+					  
+}
--- src/components/LlamagotchiMenu.js
+++ src/components/LlamagotchiMenu.js
@@ -1,32 +1,16 @@
 import React from 'react';
-
+import CareMenuItem from './CareMenuItem'
 
 
 export default class LlamagotchiMenu extends React.Component {
 	 constructor(props) { 
 		 super(props); 
-		 this.state = {
-			 menuOpen : false
-		 }
-		 this.toggle = this.toggle.bind(this);
 	 }
 	
-	toggle(e){
-		this.setState({menuOpen : ! this.state.menuOpen});
-	}
-	pet(e){
-	}
 	render() {
-		if (this.state.menuOpen) {
-			return	<div>
-						<div onClick={this.toggle}>CARE </div>
-						<ul>
-							<li onClick={this.pet}>PET</li>
-						</ul>
-					</div>
-		} else {
-			return	<div onClick={this.toggle}>CARE</div>
-		}
+		return	<div>
+					<CareMenuItem uid={this.props.uid}/>
+				</div>
 	}
 	
 					  
